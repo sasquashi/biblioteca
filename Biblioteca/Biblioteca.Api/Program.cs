4b1c3db8f79bc45c67936f3cc105c03fa66ab7d7
Jailson Ferreira Vera Junior <sasquashi@gmail.com>
2025-03-19 23:20:49 -0300

    [Biblioteca.API] - Adicionado CORS, não estava sendo visível para o Angular a API. Adicionado ainda um log de depuração que deixei comentado para poder ter usos futuros(caso seja necessário).

--- Biblioteca/Biblioteca.Api/Program.cs
+++ Biblioteca/Biblioteca.Api/Program.cs
@@ -9,7 +9,17 @@ builder.Services.AddInfrastructure(builder.Configuration.GetConnectionString("De
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -19,6 +29,17 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
+
+//log para depuração (estava tendo problemas com as requisições).
+//app.Use(async (context, next) =>
+//{
+//    Console.WriteLine($"Requisição: {context.Request.Method} {context.Request.Path}");
+//    await next.Invoke();
+//    Console.WriteLine($"Resposta: {context.Response.StatusCode}");
+//});
+
 
 app.UseAuthorization();
 
