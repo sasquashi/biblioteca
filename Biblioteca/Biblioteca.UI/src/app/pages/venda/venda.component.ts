42a218061fe8f7de5918577e1779e695bfb0e163
Jailson Ferreira Vera Junior <sasquashi@gmail.com>
2025-03-20 21:25:23 -0300

    [Biblioteca.UI] - Adicionado tela de venda.

---
+++ Biblioteca/Biblioteca.UI/src/app/pages/venda/venda.component.ts
@@ -0,0 +1,82 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { VendaService } from '../../services/venda.service';
+import { Venda } from '../../models/venda';
+import { BaseComponent } from '../../shared/component/base.component';
+import { LivroService } from '../../services/livro.service';
+import { FormaCompraService } from '../../services/forma-compra.service';
+import { FormaPagamentoService } from '../../services/forma-pagamento.service';
+import { Livro } from '../../models/livro';
+import { FormaCompra } from '../../models/forma-compra';
+import { FormaPagamento } from '../../models/forma-pagamento';
+
+@Component({
+  selector: 'app-venda',
+  templateUrl: './venda.component.html',
+  styleUrls: ['./venda.component.scss'],
+})
+export class VendaComponent extends BaseComponent<Venda> {
+  livros: Livro[] = [];
+  formasCompra: FormaCompra[] = [];
+  formasPagamento: FormaPagamento[] = [];
+
+  constructor(
+    service: VendaService,
+    router: Router,
+    private livroService: LivroService,
+    private formaCompraService: FormaCompraService,
+    private formaPagamentoService: FormaPagamentoService
+  ) {
+    super(service, router, 'venda');
+    this.selectedItem = {
+      codV: 0,
+      codFC: 0,
+      codL: 0,
+      valorLivro: 0,
+      teveDesconto: false,
+      valorFinal: 0,
+      dataVenda: new Date().toISOString().split('T')[0],
+      codFP: 0,
+    };
+  }
+
+  override ngOnInit(): void {
+    super.ngOnInit();
+    this.loadLivros();
+    this.loadFormasCompra();
+    this.loadFormasPagamento();
+    this.resetForm();
+  }
+
+  loadLivros(): void {
+    this.livroService
+      .getAll()
+      .subscribe({ next: (data) => (this.livros = data) });
+  }
+
+  loadFormasCompra(): void {
+    this.formaCompraService
+      .getAll()
+      .subscribe({ next: (data) => (this.formasCompra = data) });
+  }
+
+  loadFormasPagamento(): void {
+    this.formaPagamentoService
+      .getAll()
+      .subscribe({ next: (data) => (this.formasPagamento = data) });
+  }
+
+  addNewItem(): void {
+    this.selectedItem = {
+      codV: 0,
+      codFC: 0,
+      codL: 0,
+      valorLivro: 0,
+      teveDesconto: false,
+      valorFinal: 0,
+      dataVenda: new Date().toISOString().split('T')[0],
+      codFP: 0,
+    };
+    this.isEdit = false;
+  }
+}
